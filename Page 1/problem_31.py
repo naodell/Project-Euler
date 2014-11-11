09b4d52d82e2906aec8509ce1f42a937333725dd
Nathaniel Jay Odell <naodell@gmail.com>
2014-11-11 12:25:52 -0600

    Updating

--- Page 1/problem_31.py
+++ Page 1/problem_31.py
@@ -1,23 +1,43 @@
 '''
-In England the currency is made up of pound, £, and pence, p, and there are
+In England the currency is made up of pound, $, and pence, p, and there are
 eight coins in general circulation:
 
-1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
-It is possible to make £2 in the following way:
+1p, 2p, 5p, 10p, 20p, 50p, $1 (100p) and $2 (200p).
+It is possible to make $2 in the following way:
 
-1£1 + 150p + 220p + 15p + 12p + 31p
-How many different ways can £2 be made using any number of coins?
+1*$1 + 1*50p + 2*20p + 1*5p + 1*2p + 3*1p
+
+How many different ways can $2 be made using any number of coins?
 '''
 
-subdivisions = [1, 2, 5, 10, 20, 50, 100, 200]
-combos = []
+#from numpy import *
+import math
 
-for division in subdivisions:
-    count = 0
-    while(200-(count*division) > 0):
-        for combo in combos:
-            if (200 - (sum(combo)+count*division) > 0):
-                print count, division
-            
-        
-        
+subdivisions = [1, 2, 5, 10, 20, 50, 100, 200]
+multipliers = {1:0, 2:1}
+
+for i,subdiv1 in enumerate(subdivisions[2:]):
+    print '{0}: '.format(subdiv1),
+    nCombos = 1
+    for j,subdiv2 in enumerate(subdivisions[1:i+2]):
+        multiplier = multipliers[subdiv2]
+        nCombos += 1 + multiplier*math.floor(subdiv1/subdiv2 - 1)
+
+        print '1 + {0}*({1}/{2} - 1) = {3}; '.format(multiplier, subdiv1, subdiv2, 1 + multiplier*math.floor(subdiv1/subdiv2 - 1)),
+
+    multipliers[subdiv1] = int(nCombos)
+
+    print ''
+
+print multipliers
+
+target = 200;
+ways = (target+1)*[0]
+ways[0] = 1
+ 
+for i,subdiv1 in enumerate(subdivisions):
+    print i, subdiv1
+    for j in range(subdiv1,target+1):
+        ways[j] += ways[j - subdiv1]
+
+print dict(zip(range(target+1),ways)) 
---
+++ Page 1/problem_32.py
@@ -0,0 +1,26 @@
+'''
+We shall say that an n-digit number is pandigital if it makes use of all the
+digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1
+through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing
+multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can
+be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only
+include it once in your sum.
+'''
+
+import math
+
+pandigits = []
+for multiplicand in range(100):
+    for multiplier in range(n):
+        product = multiplier*multiplicand
+
+        digits = str(product) + str(multiplier) + str(multiplicand)
+
+        print sort(digits)
+
