164fffb05e98191ea82590d6e107b23429302cf7
Nathaniel Jay Odell <naodell@gmail.com>
2014-11-16 21:13:26 -0600

    problem 41 solved

--- Page 1/problem_41.py
+++ Page 1/problem_41.py
@@ -7,22 +7,59 @@ What is the largest n-digit pandigital prime that exists?
 '''
 import math
 
-lop   = [2]
-digits = ''.join([str(i) for i in range(1,10)])
-for number in range(3,int(math.sqrt(10000000))):
-    isPrime = True 
-    for prime in lop:
-        if prime > math.sqrt(number):
-            break
-        if number % prime == 0:
-            isPrime = False
-            break
-    
-    if isPrime:
-        lop.append(number)
+def generate_permutations(elements):
+    permutations = [[element] for element in elements]
+    while len(permutations[0]) < len(elements):
+        newPermutations = []
+        for segment in permutations:
+            for element in elements:
+                if element not in segment: 
+                    newSegment = segment[:]
+                    newSegment.append(element)
+                    newPermutations.append(newSegment)
 
-        isPandigi = False
-        if sorted(str(number)) == digits:
-            print number
-            isPandigi = True
-            
+        permutations = newPermutations
+
+    return permutations
+
+def generate_primes(upperBound):
+    # Generates all primes up to some upperbound
+    lop   = [2]
+    for number in range(3,upperBound):
+        isPrime = True 
+        for prime in lop:
+            if prime > math.sqrt(number):
+                break
+            if number % prime == 0:
+                isPrime = False
+                break
+        
+        if isPrime:
+            lop.append(number)
+
+    return lop
+
+#Generate all primes less than sqrt(Billion)
+lop = generate_primes(int(math.sqrt(1e10)))
+
+#Check if pandigits are prime
+maxPandigit = 0
+for order in range(3,10):
+    digits = ''.join([str(i) for i in range(1,order)])
+    pandigits = generate_permutations(digits)
+
+    #Find largest prime pandigit
+    for pandigit in pandigits:
+        number = int(''.join(pandigit))
+        isPrime = True
+        for prime in lop:
+            if prime > math.sqrt(number):
+                break
+            if number % prime == 0:
+                isPrime = False
+                break
+
+        if isPrime and number > maxPandigit:
+            maxPandigit = number
+
+print 'The largest prime, n-digit pandigital number is {0}'.format(maxPandigit)
